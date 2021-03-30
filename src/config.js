076153b5f1a76bb663ec11c5f8fc3c399c9a0083
poutingemoji <9awu680@gmail.com>
2021-03-30 13:36:56 -0700

    production

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   BACKEND_ROOT_URL: false
     ? `http://localhost:3001`
     : "https://pfp-logger-backend.herokuapp.com",
-  CLIENT_ROOT_URL: true
+  CLIENT_ROOT_URL: false
     ? `http://localhost:3000`
     : "https://poutingemoji.github.io/pfp-logger-client",
 };
