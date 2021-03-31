ad1a7bd604c3672f51200bab1bd8fa87b050a9d4
poutingemoji <9awu680@gmail.com>
2021-03-31 04:08:51 -0700

    updated backend url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   BACKEND_ROOT_URL: false
-    ? `http://localhost:3001`
-    : "https://pfp-logger-backend.herokuapp.com",
-  CLIENT_ROOT_URL: false
-    ? `http://localhost:3000`
-    : "https://pfp-logger.netlify.app",
+    ? "http://localhost:3001"
+    : "http://api.pfp-logger.xyz",
+  CLIENT_ROOT_URL: false ? "http://localhost:3000" : "http://pfp-logger.xyz",
 };
