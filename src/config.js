acee3c7f07b6522327565a9b015b3456e781f899
poutingemoji <9awu680@gmail.com>
2021-03-31 04:24:08 -0700

    production url updated to http://pfp-logger.xyz

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BACKEND_ROOT_URL: false
     ? "http://localhost:3001"
-    : "http://api.pfp-logger.xyz",
+    : "http://pfp-logger.xyz",
   CLIENT_ROOT_URL: false ? "http://localhost:3000" : "http://pfp-logger.xyz",
 };
