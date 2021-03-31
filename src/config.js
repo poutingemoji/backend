3f33dc4417dd28666650d8aa136835d8b9f9b85b
poutingemoji <9awu680@gmail.com>
2021-03-31 04:39:13 -0700

    changed api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BACKEND_ROOT_URL: false
     ? "http://localhost:3001"
-    : "http://pfp-logger.xyz",
+    : "http://api.pfp-logger.xyz",
   CLIENT_ROOT_URL: false ? "http://localhost:3000" : "http://pfp-logger.xyz",
 };
