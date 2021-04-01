e6ea02e93d7e0e8c612db1355d21eb6c37dfe6f3
poutingemoji <9awu680@gmail.com>
2021-03-31 20:20:15 -0700

    https => http

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BACKEND_ROOT_URL: false
     ? "http://localhost:3001"
-    : "https://api.pfp-logger.xyz",
+    : "http://api.pfp-logger.xyz",
   CLIENT_ROOT_URL: false ? "http://localhost:3000" : "https://pfp-logger.xyz",
 };
